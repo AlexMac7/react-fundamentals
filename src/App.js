c32391f2e026c95772f4c6b1d04369bcacfab517
Alex MacKenzie <alexbcmac7@gmail.com>
2018-05-09 17:29:26 -0700

    Sets props for app component with types

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
     render () {
+        let text = this.props.text;
+        let cat = this.props.cat;
+
         return (
             //wrap jsx elements in a parent node to return multiple nodes
             <div>
-                <h1 className="">Hello World</h1>
-                <h1 className="">Test a second one</h1>
+                <h1 className="">{text}</h1>
+                <h1 className="">{`I have ${cat} cats`}</h1>
             </div>
         );
     }
+}
+
+App.propTypes = {
+    text: PropTypes.string,
+    cat: PropTypes.number.isRequired,
 }
 
 //stateless function component below, obviously can't have state like the class component
